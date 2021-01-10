c8d40c17b5c513731ec15518b281631a2b986a1e
sauravk1996 <skbk104@gmail.com>
2021-01-10 21:50:31 +0530

    Added Axios to project

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,15 @@ namespace API
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:3000");
+        });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +59,8 @@ namespace API
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
 
-      app.UseHttpsRedirection();
+      // app.UseHttpsRedirection();
+      app.UseCors("CorsPolicy");
 
       app.UseRouting();
 
